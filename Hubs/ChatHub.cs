854a8fea0740ca61dd49d13115e44c47d2197309
Gadi0553 <157258544+Gadi0553@users.noreply.github.com>
2024-10-29 00:19:14 -0400

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AspNetCoreSignalRDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task EditMessage(string user, string oldMessage, string newMessage)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, newMessage);
+            // Aquí podrías almacenar el nuevo mensaje en una base de datos o estructura.
+        }
+
+       
+    }
+
+
+}
+
+
